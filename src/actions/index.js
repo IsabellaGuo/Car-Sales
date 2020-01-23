8bb7fa34fa022ec18e6e2bfe6b3d3bc525cd38b7
IsabellaGuo <isabella.guofei@gmail.com>
2020-01-23 14:14:32 -0800

    First draft is done, need to keep working on the rest.

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+//action index
+
+export const addFeature = (newFeature) => {
+    return { type: 'ADD_FEATURE', payload: newFeature }
+}
+
+export const removeFeature = (feature) => {
+    return { type: 'REMOVE_FEATURE', payload: feature }
+}
